226c5767c31040cc58198cccf255077e84fe1cd1
Henrique Neubert <neubert.study@gmail.com>
2022-01-21 08:59:34 -0300

    0.3

---
+++ apostila_algoritmos_mar2007.js
@@ -0,0 +1,81 @@
+//exercicio do exemplo 8 (apostila_algoritmos_mar2007)
+      // let a = 2;
+      // let b = 1;
+
+      // let result = a + b;
+      // console.log(result);
+
+      // console.log('Novo Cálculo? S/N');
+      // let resposta = true; 
+      // if(resposta = true){
+      //       let result = a + b;
+      //       console.log(result);
+      // }else{
+      //       console.log('Fim de Cálculo');
+      // }
+//exercicio do exemplo 8 (apostila_algoritmos_mar2007)
+      
+//exercicio do exemplo 9 (apostila_algoritmos_mar2007)
+
+// let alunos = [
+//       aluno1[5.7],
+//       aluno2[6.5],
+//       aluno3[9]
+// ]
+
+//exercicio do exemplo 9 (apostila_algoritmos_mar2007)
+
+//exercicio do exemplo 11 (apostila_algoritmos_mar2007)
+
+      // let aluno1 = nota = 8;
+      // let aluno2 = nota = 2;
+      // let  aluno3 = nota = 10;
+      // let aluno4 = nota = 9;
+      // let aluno5 = nota = 3;
+      // let aluno6 = nota = 8;
+      // let aluno7 = nota = 7;
+      // let aluno8 = nota = 1;
+      // let aluno9 = nota = 4;
+      // let aluno10 = nota = 0;
+
+      // let alunos = [
+      //       'aluno1 = nota = 8;',
+      //       'aluno2 = nota = 2;',
+      //       ' aluno3 = nota = 10;',
+      //       'aluno4 = nota = 9;',
+      //       'aluno5 = nota = 3;',
+      //       'aluno6 = nota = 8;',
+      //       'aluno7 = nota = 7;',
+      //       'aluno8 = nota = 1;',
+      //       'aluno9 = nota = 4;',
+      //       'aluno10 = nota = 0;'
+      // ];
+
+      // console.log(alunos)
+
+      // for (i = 0;  i <= 10;  i++ ){
+      
+      
+      // }
+
+      //NT
+
+//exercicio do exemplo 11 (apostila_algoritmos_mar2007)
+
+//exercicio do exemplo 13 (apostila_algoritmos_mar2007)
+
+// soma = 0;
+// media = 0;
+// cont = 0;
+
+// for( x = 1; x <= 30; x++ ){
+//       console.log(x);
+// }
+
+//exercicio do exemplo 13 (apostila_algoritmos_mar2007)
+
+
+
+      // console.log('page 18.6');
+      //loops
+      // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Loops_and_iteration
---
+++ exerc_resp_alg_mar2007.js
@@ -0,0 +1,17 @@
+//exercicio do exemplo1 (exerc_resp_alg_mar2007)
+
+var a = 2;
+var b = 3;
+
+if( a = 2  ){
+      a = b;
+      console.log(a);
+}
+
+if(b = 3){
+      b = a;
+      console.log(b);
+}
+
+
+//exercicio do exemplo1 (exerc_resp_alg_mar2007)
