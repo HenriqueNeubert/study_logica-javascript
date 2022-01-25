11eb0a35341cd00e71d142318b7c3efa84027c27
Henrique Neubert <neubert.study@gmail.com>
2022-01-25 08:58:10 -0300

    0.6

--- exerc_resp_alg_mar2007.js
+++ exerc_resp_alg_mar2007.js
@@ -32,12 +32,80 @@
 
 //(B)
 
-a = 30;
-b= 20;
-c = a + b;
-console.log(c);
+// a = 30;
+// b= 20;
+// c = a + b;
+// console.log(c); // 50
+// b = 10;
+// console.log(b, c); //10 50 
+// c = a + b;
+// console.log(a, b, c);//30 10 40
 
 //(B)
 
+//(C)
+
+      // a = 10;
+      // b = 20;
+      // c = a;
+      // b = c;
+      // a = b;
+      // console.log(a, b, c);// 20 10 10 errei
+
+//(C)
+
+//(D)
+
+// a = 10;
+// b = a + 1;//11
+// a = b + 1;//12
+// b = a + 1;//13 resposta
+// console.log(a); //12 acertei
+// a = b + 1;//14
+// console.log(a, b); //12 12 errei os dois
+
+//(D)
+
+//(E)
+
+      // a = 10;
+      // b =5;
+      // c = a + b;
+      // b = 20;
+      // a = 10;
+      // console.log(a, b ,c);//10 20 15
+
+//(E)
+
+//(F)
+
+      // x = 1;
+      // y = 2;
+      // z = y - x;
+      // console.log(z);//1
+      // x = 5;
+      // y = x + z;
+      // console.log(x, y ,z);//5 6 1
+
+//(F)
+
 //exercicio do exemplo 2 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 3 (exerc_resp_alg_mar2007)
+
+// a1 = ( 4 / 2 ) + ( 2 / 4 ); //2.5 acerteri
+// a2 =  4 / 2 + 2 / 4; //2.5 acerteri
+// console.log(a1, a2);
+
+// b1 = 4 / ( 2 + 2 ) / 4; //4 errei 0.25 sempre da esquerda para a direita
+// b2 = 4 / 2 + 2 / 4; //2.5 acerteri
+// console.log(b1, b2); 
+
+// c1 = ( 4 + 2 ) * 2 - 4; //8 acerteri
+// c2 = 4 + 2 * 2 - 4; //8 errei 4 primeiro a multiplicação
+// console.log(c1, c2); 
+
+//exercicio do exemplo 3 (exerc_resp_alg_mar2007)
+
+
 
