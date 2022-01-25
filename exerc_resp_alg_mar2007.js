38760b080d86c15e3769336ff76ee89c6f58bb7f
Henrique Neubert <neubert.study@gmail.com>
2022-01-25 06:54:20 -0300

    0.4

--- exerc_resp_alg_mar2007.js
+++ exerc_resp_alg_mar2007.js
@@ -1,17 +1,43 @@
-//exercicio do exemplo1 (exerc_resp_alg_mar2007)
+//exercicio do exemplo 1 (exerc_resp_alg_mar2007)
 
-var a = 2;
-var b = 3;
+// var a = 2;
+// var b = 3;
 
-if( a = 2  ){
-      a = b;
-      console.log(a);
-}
+// if( a == 2 ){
+//       a = b
+//       console.log(a);
+// }
+//       a = 2;
 
-if(b = 3){
-      b = a;
-      console.log(b);
-}
+// if( b == 3 ){
+//       b = a
+//       console.log(b);
+// }
 
+//exercicio do exemplo 1 (exerc_resp_alg_mar2007)
 
-//exercicio do exemplo1 (exerc_resp_alg_mar2007)
+//exercicio do exemplo 2 (exerc_resp_alg_mar2007)
+
+//(A)
+
+// a = 10;
+// b = 20;
+// console.log(b)
+// b = 5;
+// console.log(a , b);
+
+// RESPOSTA : 20 , 10 , 5
+
+//(A)
+
+//(B)
+
+a = 30;
+b= 20;
+c = a + b;
+console.log(c);
+
+//(B)
+
+//exercicio do exemplo 2 (exerc_resp_alg_mar2007)
+
