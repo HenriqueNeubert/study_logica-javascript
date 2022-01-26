43bf3b742417d70a194c1a15b59de96f41948276
Henrique Neubert <neubert.study@gmail.com>
2022-01-26 08:50:01 -0300

    0.7

--- exerc_resp_alg_mar2007.js
+++ exerc_resp_alg_mar2007.js
@@ -37,7 +37,7 @@
 // c = a + b;
 // console.log(c); // 50
 // b = 10;
-// console.log(b, c); //10 50 
+// console.log(b, c); //10 50
 // c = a + b;
 // console.log(a, b, c);//30 10 40
 
@@ -99,13 +99,94 @@
 
 // b1 = 4 / ( 2 + 2 ) / 4; //4 errei 0.25 sempre da esquerda para a direita
 // b2 = 4 / 2 + 2 / 4; //2.5 acerteri
-// console.log(b1, b2); 
+// console.log(b1, b2);
 
 // c1 = ( 4 + 2 ) * 2 - 4; //8 acerteri
 // c2 = 4 + 2 * 2 - 4; //8 errei 4 primeiro a multiplicação
-// console.log(c1, c2); 
+// console.log(c1, c2);
 
 //exercicio do exemplo 3 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 4 (exerc_resp_alg_mar2007)
+
+// a = 6 * ( 3 + 2 );
+// a = 3 + 2;
+// a = a * 6;
+//  console.log(a);
+
+// b = 2 + ( 6 * ( 3 + 2 ) );
+// console.log(b);
+// b = 3 + 2;
+// b = 6 * b;
+// b = 2 + b;
+// console.log(b);
+
+// c = 2 + ( 3 * 6 ) / ( 2 + 4 );
+// console.log(c);
+// c = 2 + 4;
+// c = 3 * 6 / c;
+// c = c + 2;
+// console.log(c);
+
+// d = 2 * ( 8 / 3 + 1 );
+// console.log(d);
+// d = 8 / 3 + 1;
+// d = 2 * d;
+// console.log(d);
+
+// e = 3 + ( 16 - 2 ) / ( 2 * ( 9 - 2 ) ); //consegui fazer com apenas 1 parenteses
+// console.log(e);
+// e = 9 - 2;
+// e = e * 2;
+// e = ( 16 - 2 ) / e;
+// e = e + 3;
+// console.log(e);
+
+// f = ( 6 / 3 ) + ( 8 / 2 );
+// console.log(f);
+// f = 6 / 3 + 8 / 2;
+// console.log(f);
+
+// g = ( ( 3 + ( 8 / 2 ) ) * 4 ) + ( 3 * 2 );
+// console.log(g);
+// g = 8 / 2 + 3;
+// g = g * 4;
+// g = g + 3 * 2;
+// console.log(g);
+
+// h = ( 6 * ( 3 * 3 ) + 6 ) - 10;
+// console.log(h);
+// h = 3 * 3;
+// h = h * 6 + 6;
+// h = h - 10;
+// console.log(h);
+
+// i = ( ( ( 10 * 8 ) + 3 ) * 9 );
+// console.log(i);
+// i = 10 * 8 + 3;
+// i = i * 9; 
+// console.log(i);
+
+// j = ( ( - 12 ) * ( -4 ) ) * ( 3 * ( - 4 ) );
+// console.log(j);
+// j = -12 * -4;
+// j = 3 * -4 * j;
+// console.log(j);
+
+//exercicio do exemplo 4 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 5 (exerc_resp_alg_mar2007)
+
+
+
+//exercicio do exemplo 5 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 6 (exerc_resp_alg_mar2007)
+
+
+
+//exercicio do exemplo 6 (exerc_resp_alg_mar2007)
+
 
 
 
