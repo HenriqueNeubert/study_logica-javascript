8c1246c01a782a983dedd4920823b2af07eae280
Henrique Neubert <neubert.study@gmail.com>
2022-02-09 22:18:47 -0300

    0.9

--- exerc_resp_alg_mar2007.js
+++ exerc_resp_alg_mar2007.js
@@ -1,5 +1,5 @@
 //EXERCICIOS QUE EU NÃO FIZ
-// 5 - 
+// 5 - 11 -
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -169,7 +169,7 @@
 // i = ( ( ( 10 * 8 ) + 3 ) * 9 );
 // console.log(i);
 // i = 10 * 8 + 3;
-// i = i * 9; 
+// i = i * 9;
 // console.log(i);
 
 // j = ( ( - 12 ) * ( -4 ) ) * ( 3 * ( - 4 ) );
@@ -258,7 +258,7 @@
 //exercicio do exemplo 10 (exerc_resp_alg_mar2007)
 
 // var porcDistribuidor = 28; //%
-// var impostos = 45;//% 
+// var impostos = 45;//%
 
 // var custoFabrica = porcDistribuidor + impostos;
 // console.log('Porcentual do Custo de Fábrica é de ' + ( 100 - custoFabrica ) + '%');
@@ -269,11 +269,222 @@
 
 //exercicio do exemplo 11 (exerc_resp_alg_mar2007)
 
-var salario = 0;
-var comissao = 0;
-var valVendas = 5;//% 
+// var salario = 0; //$?
+// var comissaoCarro = 0;//$?
+// var comissaoValor = 5; //%
+// var carrosVendidos = 5;//Question
+// var valorTotalVendas = 0;//Question
+// var valorCarroVendido = 0;//
+// var salarioFinal = 0;//Question
+
 
 //exercicio do exemplo 11 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 12 (exerc_resp_alg_mar2007)
+
+// var celcius = 100;
+// var farenheit = '';
+
+// if ( farenheit == '' ){
+//       var farenheit = ( celcius / 5 ) * 9 + 32;
+//       console.log(farenheit);
+// }else{
+//       var celcius = ( farenheit - 32 ) / 9 * 5;
+//       console.log(celcius);
+// }
+
+//exercicio do exemplo 12 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 13 (exerc_resp_alg_mar2007)
+
+// var nota1 = 5;
+// var nota2 = 8;
+// var nota3 = 2;
+
+// var mediaFinal = ( nota1 * 2 + nota2 * 3 + nota3 * 5 ) / 10; 
+// console.log(mediaFinal);
+
+//exercicio do exemplo 13 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 14 (exerc_resp_alg_mar2007)
+
+// var valor = 1;
+
+// if( valor > 10 ){
+//       console.log('SIM EU SOU MAIOR QUE 10,' + ' EU SOU ' + valor);
+// }else{
+//       console.log('NÃO EU NÃO SOU MAIOR QUE 10,' + ' EU SOU ' + valor)
+// }
+
+//exercicio do exemplo 14 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 15 (exerc_resp_alg_mar2007)
+
+// var valor = 0;
+
+// if( valor < 0 ){
+//       console.log('SOU UM NÚMERO NEGATIVO, EU SOU = ' + valor);
+// }else{
+//       console.log('SOU UM NÚMERO POSITIVO, EU SOU = ' + valor);
+// }
+
+//exercicio do exemplo 15 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 16 (exerc_resp_alg_mar2007)
+
+// var macas = 13;
+// var valor = 1.30;
+
+// if( macas <= 6 ){
+//       var result = macas * valor;
+//       console.log('Total de sua compra é = ' + result + 'R$ Sendo que cada maçã saiu por ' + valor + 'R$' );
+// }
+
+// if( macas >= 12 ){
+//       console.log(macas);
+//       var valor = 1.00;
+//       var result = valor * macas;
+//       console.log('Total de sua compra é = ' + result + 'R$ Sendo que cada maçã saiu por ' + valor + 'R$' );
+// }
+
+//exercicio do exemplo 16 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 17 (exerc_resp_alg_mar2007)
+
+// var prova1 = 5.1;
+// var prova2 = 6.8;
+// var media = ( prova1 + prova2 ) / 2;
+
+// if ( media >= 6 ){
+//       var result = parseFloat(media.toFixed(2)); //mostra apenas 2 casas depois da virgula
+//       console.log('Você foi aprovado, sua média foi ' + result);
+// }else{
+//       var result = parseFloat(media.toFixed(2)); //mostra apenas 2 casas depois da virgula
+//       console.log('Você foi reprovado, sua média foi ' + result );
+// }
+
+//exercicio do exemplo 17 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 18 (exerc_resp_alg_mar2007)
+
+// var anoAtual =  2022;
+// var anoNasc = 2004;
+// var validacao = anoAtual - anoNasc;
+
+// if( validacao <= 17 ){
+//       console.log( 'Sua idade é ' + validacao + ', sendo assim, não apto a votar neste ano.' );
+// }else{
+//       console.log( 'Sua idade é ' + validacao + ', sendo assim, apto a votar neste ano.' );
+// }
+
+//exercicio do exemplo 18 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 19 (exerc_resp_alg_mar2007)
+
+// var valorA = 5;
+// var valorB = 3;
+
+// if( valorA == valorB ){
+//       console.log( 'Não se adequa.' );
+// }
+// if ( valorA > valorB ){
+//       console.log('O valor A é maior que B: A = ' + valorA + ', B = ' + valorB + '.');
+// } 
+// if( valorB > valorA ){
+//       console.log('O valor B é maior que A: A = ' + valorA + ', B = ' + valorB + '.');
+// } 
+
+//exercicio do exemplo 19 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 20 (exerc_resp_alg_mar2007)
+
+// var valorA = 3;
+// var valorB = 3;
+
+// if( valorA == valorB ){
+//       console.log( 'Não se adequa.' );
+// }
+
+// if( valorB < valorA ){
+//       console.log('valor B = ' +  valorB +  ' valor A = ' + valorA);
+// }
+
+// if( valorB > valorA ){
+//       console.log('valor A = ' +  valorA +  ' valor B = ' + valorB);
+// }
+
+//exercicio do exemplo 20 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 21 (exerc_resp_alg_mar2007)
+
+// var inicio = 20;
+// var fim = 18;
+// var duracao = fim - inicio;
+// var maximo = 24;
+
+// if( fim == inicio){
+//       console.log('A duração da partida foi de ' + 24 + ' horas');
+// }
+
+// if( duracao > maximo ){
+//       console.log('O tempo excedeu o limite máximo.');
+// }
+
+// if( fim > inicio ){
+//       console.log('A duração da partida foi de ' + duracao + ' horas');
+// }
+
+// if ( fim < inicio ) {
+//       var duracao = (maximo - inicio) + fim;
+//       console.log('A duração da partida foi de ' + duracao + ' horas');
+// }
+
+//exercicio do exemplo 21 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 22 (exerc_resp_alg_mar2007)
+
+
+
+//exercicio do exemplo 22 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 23 (exerc_resp_alg_mar2007)
+
+//erros
+//altura não declarada
+//peso deveria estar em ula variavel
+
+var sexo = 'F';
+var altura = 1.70;
+var peso = 63;
+
+var formMasc = (peso * altura) - 58;
+var formFem = (peso * altura) - 44.7;
+
+if( sexo == 'M' ){
+      var pesoIdeal =  formMasc;
+}else{
+      var pesoIdeal =  formFem;
+}
+
+console.log(pesoIdeal = parseFloat(pesoIdeal.toFixed(2)) );
+
+
+//exercicio do exemplo 23 (exerc_resp_alg_mar2007)
+
+//exercicio do exemplo 24 (exerc_resp_alg_mar2007)
+
+      
+
+//exercicio do exemplo 24 (exerc_resp_alg_mar2007)
+
+
+
+
+
+
+
+
+
 
 
 
